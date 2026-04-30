9dc7522073751162b27d16f8ad2c1fe22479c475
Giotsche <gioele.petrillo@gmail.com>
2026-04-30 15:04:32 +0200

    new ashampoo version

--- ashampoo-app/tools/chocolateyinstall.ps1
+++ ashampoo-app/tools/chocolateyinstall.ps1
@@ -1,24 +1,25 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://cdn1.ashampoo.net/ashampoo/0401/ashampoo_app_2.13.0.exe'
-$url64      = 'https://cdn1.ashampoo.net/ashampoo/0401/ashampoo_app_2.13.0.exe'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://cdn1.ashampoo.net/ashampoo/0401/ashampoo_app_2.16.1.exe'
+$url64 = 'https://cdn1.ashampoo.net/ashampoo/0401/ashampoo_app_2.16.1.exe'
+$checksum = '97DB2D19D32BF4535F8C32D1A21B7A90B4F9E28B25457B3A20C62F2D0D908DF5'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  url64bit      = $url64
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  url64bit       = $url64
 
-  softwareName  = 'ashampoo-app*'
+  softwareName   = 'ashampoo-app*'
 
-  checksum      = '4ECE417173413AF71DCFE8920D18352ACD4C3ACE8897B3DCA11915F149EFBBF9'
-  checksumType  = 'sha256'
-  checksum64    = '4ECE417173413AF71DCFE8920D18352ACD4C3ACE8897B3DCA11915F149EFBBF9'
-  checksumType64= 'sha256'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  checksum64     = $checksum
+  checksumType64 = 'sha256'
 
-  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log`""
-  validExitCodes= @(0)
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log`""
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
